495c80580a320e2627f5f40089b757308cc7febb
sahit <19135A0418@gvpce.ac.in>
2022-03-26 19:48:33 +0530

    Revert "added age to Person class"
    This reverts commit 6d985913d3fb3b778d211b8d02c820642d769952.

--- src/main/java/com/example/sahitya/old/Person.java
+++ src/main/java/com/example/sahitya/old/Person.java
@@ -2,18 +2,11 @@ package com.example.sahitya.old;
 
 public class Person {
    private String name;
-   private int age;
     public String getName(){
         return name;
     }
     public void setName(String name){
-        this.name =name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
+        this.name = name;
     }
 }
 
