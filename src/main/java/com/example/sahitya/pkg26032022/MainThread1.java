3bc42399d76abf1ec53d83c7bd046d5d4bfe6f9c
sahit <19135A0418@gvpce.ac.in>
2022-03-28 09:35:03 +0530

    log4j2 changes

--- src/main/java/com/example/sahitya/pkg26032022/MainThread1.java
+++ src/main/java/com/example/sahitya/pkg26032022/MainThread1.java
@@ -1,18 +1,23 @@
 package com.example.sahitya.pkg26032022;
 
-import java.lang.Thread;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-    public class MainThread1 implements Runnable {
-        public static void main(String[] args) {
-            MainThread1 obj = new MainThread1();
-            Thread thread = new Thread(obj);
-            thread.start();
-            System.out.println("code is outside of the threadd");
-        }
-        public void run() {
-            System.out.println(" code is running in a thread");
-        }
+public class MainThread1 implements Runnable {
+    private static final Logger logger = LogManager.getLogger(MainThread.class);
+
+    public static void main(String[] args) {
+        MainThread1 obj = new MainThread1();
+        Thread thread = new Thread(obj, "Sahitya-Thread");
+        thread.start();
+        //System.out.println("code is outside of the thread");git
+        logger.info("code is outside of the thread");
     }
+
+    public void run() {
+        logger.info(" code is running in a thread");
+    }
+}
 
 
 
